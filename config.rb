5a8b8b6f3df032d9f25cbf8868407bab17f2b2ec
Cameron Prebble <cameron.prebble@abletech.co.nz>
2015-10-03 16:38:42 +0200

    initial commit

---
+++ config.rb
@@ -0,0 +1,49 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+page '/*.csv', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Reload the browser automatically whenever files change
+configure :development do
+  # activate :livereload
+
+  activate :external_pipeline,
+    name: :browserify,
+    command: "node_modules/.bin/watchify source/javascripts/index.js -v -d -t babelify --outfile source/javascripts/index.pkg.js",
+    source: "source/javascripts",
+    latency: 2
+end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+end
